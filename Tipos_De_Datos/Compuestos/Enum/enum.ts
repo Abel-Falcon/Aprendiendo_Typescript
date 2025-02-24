faf0a0ee5ebbd8e6cf67b8f6a9adb2d69971bc28
Abel-Falcon <nehemias.falcon@upeu.edu.pe>
2025-02-24 17:29:26 -0500

    Aprendiendo sobre datos compuestos "Enums" en Typescript

---
+++ Tipos_De_Datos/Compuestos/Enum/enum.ts
@@ -0,0 +1,19 @@
+// Enum
+
+//Definición de un Enum para días de la semana
+enum DiasSemana {
+  Lunes,
+  Martes,
+  Miercoles,
+  Jueves,
+  Viernes,
+  Sabado,
+  Domingo,
+}
+
+//Enum con valores de cadena(string Enums)
+enum colores {
+  Rojo = "Rojo",
+  Verde = "Verde",
+  Azul = "Azul",
+}
