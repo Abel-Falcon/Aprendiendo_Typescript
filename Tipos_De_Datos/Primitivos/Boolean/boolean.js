f570aae48f7fd8470dc21dcdbe8103a0cfb3dfa1
Abel-Falcon <nehemias.falcon@upeu.edu.pe>
2025-02-24 15:00:15 -0500

    Aprendiendo sobre datos primitivos "Booleans" en Typescript

---
+++ Tipos_De_Datos/Primitivos/Boolean/boolean.js
@@ -0,0 +1,4 @@
+"use strict";
+const bool1 = true;
+const bool2 = false;
+//Importante, primiero haces la declaración de la variable y luego la asignación.
---
+++ Tipos_De_Datos/Primitivos/Boolean/boolean.ts
@@ -0,0 +1,4 @@
+const bool1: boolean = true;
+const bool2: boolean = false;
+
+//Importante, primiero haces la declaración de la variable y luego la asignación.
