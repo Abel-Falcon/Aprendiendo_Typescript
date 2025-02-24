f83700f160acaf2ec965d5abe9e6647fe668c65d
Abel-Falcon <nehemias.falcon@upeu.edu.pe>
2025-02-24 14:20:43 -0500

    Aprendiendo sobre datos primitivos String  en Typescript

---
+++ Tipos_De_Datos/Primitivos/String/string.js
@@ -0,0 +1,11 @@
+"use strict";
+//String
+//Comillas Dobles:
+const str1 = "Hola, soy una cadena con comillas dobles";
+//Comillas Simples:
+const str2 = "Hola, soy una cadena con comillas simples";
+//Template Literals (Backticks):
+const nombre = "Abel";
+const edad = 19;
+const str3 = `Hola, mi nombre es ${nombre} y tengo ${edad} años`;
+console.log(str3);
