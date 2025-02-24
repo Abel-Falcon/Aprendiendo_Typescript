af354a5aea730802f08de73783d38458ae24ae4a
Abel-Falcon <nehemias.falcon@upeu.edu.pe>
2025-02-24 15:47:02 -0500

    Aprendiedo sobre datos primitivos "Null" en Typescript

---
+++ Tipos_De_Datos/Primitivos/Null/null.js
@@ -0,0 +1,6 @@
+"use strict";
+//NULL
+//Declaración de una variable con valor null
+let variableNull;
+//Asignación de valor null
+variableNull = null;
---
+++ Tipos_De_Datos/Primitivos/Null/null.ts
@@ -0,0 +1,7 @@
+//NULL
+
+//Declaración de una variable con valor null
+let variableNull: null;
+
+//Asignación de valor null
+variableNull = null;
